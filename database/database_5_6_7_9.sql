c4ab01f28c608546c6596e6ab590918ab922f7d7
Benjamin <carlosbpac@gmail.com>
2025-05-01 18:22:03 -0600

    feat: Add configuration files and update dependencies for services

---
+++ database/database_5_6_7_9.sql
@@ -0,0 +1,17 @@
+-- Eliminar la base de datos si existe
+DROP DATABASE IF EXISTS 5_6_7_9_ayd2_db;
+
+-- Crear la base de datos
+CREATE DATABASE 5_6_7_9_ayd2_db;
+
+-- Usar la base de datos
+USE 5_6_7_9_ayd2_db;
+
+-- Verificar si el usuario no existe y crearlo
+CREATE USER IF NOT EXISTS 'user_5_6_7_9_ayd2_db'@'127.0.0.1' IDENTIFIED BY '123';
+
+-- Otorgar permisos específicos sobre la base de datos
+GRANT ALL PRIVILEGES ON 5_6_7_9_ayd2_db.* TO 'user_5_6_7_9_ayd2_db'@'127.0.0.1';
+
+-- Aplicar cambios de permisos
+FLUSH PRIVILEGES;
