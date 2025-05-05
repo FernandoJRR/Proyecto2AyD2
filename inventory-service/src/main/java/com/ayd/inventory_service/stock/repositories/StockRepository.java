10a797d4746409195cfa263a4ce9ea40fbf464b3
Benjamin <carlosbpac@gmail.com>
2025-05-05 00:35:17 -0600

    feat: Add StockRepository for managing stock data with methods for checking product availability and querying low stock items

---
+++ inventory-service/src/main/java/com/ayd/inventory_service/stock/repositories/StockRepository.java
@@ -0,0 +1,18 @@
+package com.ayd.inventory_service.stock.repositories;
+
+import com.ayd.inventory_service.stock.models.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface StockRepository extends JpaRepository<Stock,String> {
+    boolean existsByProductIdAndWarehouseId(String productId, String warehouseId);
+    List<Stock> findByWarehouseId(String warehouseId);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM stock s WHERE s.quantity <= s.minimum_stock AND s.warehouse_id = ?1")
+    List<Stock> findProductsLowStockByWarehouseId(String warehouseId);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM stock s WHERE s.quantity <= s.minimum_stock")
+    List<Stock> findProductsLowStock();
+}
