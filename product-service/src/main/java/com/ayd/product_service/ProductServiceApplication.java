41b38704ec339b66cc041730f74ac4e788d9a462
Luis Monterroso <elrincondelgamer77@gmail.com>
2025-05-10 01:20:52 -0600

    Centralizando filtro de seguridad

--- product-service/src/main/java/com/ayd/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/ayd/product_service/ProductServiceApplication.java
@@ -2,14 +2,18 @@ package com.ayd.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import com.ayd.shared.SwaggerConfig;
+import com.ayd.shared.swagger.SwaggerConfig;
 import com.ayd.shared.handlers.GlobalExceptionHandler;
+import com.ayd.shared.security.AppProperties;
+import com.ayd.shared.security.SecurityConfig;
 
 @SpringBootApplication
-@Import({ GlobalExceptionHandler.class, SwaggerConfig.class })
+@Import({ GlobalExceptionHandler.class, SwaggerConfig.class, SecurityConfig.class })
+@EnableConfigurationProperties(AppProperties.class)
 @EnableJpaAuditing
 public class ProductServiceApplication {
 
