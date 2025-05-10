5ef5fc0215ce443b4e5d16f92437e66f0123a1c9
Luis Monterroso <elrincondelgamer77@gmail.com>
2025-05-10 00:07:12 -0600

    agregando libreria

---
+++ common-lib/src/main/java/com/ayd/config/AuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.ayd.config;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AuthenticationFilter extends OncePerRequestFilter {
+    /**
+     * Método principal que intercepta cada solicitud HTTP y ejecuta la
+     * validación del token JWT.
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String username = request.getHeader("auth-user");
+        String rolesHeader = request.getHeader("auth-permissions");
+
+        if (username != null && rolesHeader != null && !rolesHeader.isEmpty()) {
+            List<GrantedAuthority> authorities = Stream.of(rolesHeader.split(","))
+                    .map(permisson -> permisson.trim())
+                    .map(permisson -> new SimpleGrantedAuthority(permisson))
+                    .collect(Collectors.toList());
+
+            Authentication auth = new UsernamePasswordAuthenticationToken(username, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
