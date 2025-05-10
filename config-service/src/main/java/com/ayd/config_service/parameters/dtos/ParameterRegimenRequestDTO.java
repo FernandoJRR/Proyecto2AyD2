93a9121f685b3180dc9a68b725debc554a5ba6b1
FernandoJRR <fercitotron@gmail.com>
2025-05-10 00:19:39 -0600

    feat: add more endpoints

---
+++ config-service/src/main/java/com/ayd/config_service/parameters/dtos/ParameterRegimenRequestDTO.java
@@ -0,0 +1,15 @@
+package com.ayd.config_service.parameters.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class ParameterRegimenRequestDTO {
+    @NotBlank(message = "El nuevo tipo de regimen es obligatorio")
+    @Pattern(regexp = "gen|peq", message = "El valor del nuevo regimen debe ser 'gen' o 'peq'")
+    private String nuevoRegimen;
+}
