adddd7f665d6474ee85ce5406bb154fa68f182ab
Luis Monterroso <elrincondelgamer77@gmail.com>
2025-05-11 20:09:26 -0600

    Agregado el web client a inventory service e invoice service

--- inventory-service/src/main/java/com/ayd/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ayd/inventory_service/InventoryServiceApplication.java
@@ -9,11 +9,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import com.ayd.shared.handlers.GlobalExceptionHandler;
 import com.ayd.shared.security.AppProperties;
 import com.ayd.shared.security.SecurityConfig;
+import com.ayd.shared.security.WebClientConfig;
 import com.ayd.shared.swagger.SwaggerConfig;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@Import({ GlobalExceptionHandler.class, SwaggerConfig.class, SecurityConfig.class })
+@Import({ GlobalExceptionHandler.class, SwaggerConfig.class, SecurityConfig.class, WebClientConfig.class })
 @EnableConfigurationProperties(AppProperties.class)
 public class InventoryServiceApplication {
 
