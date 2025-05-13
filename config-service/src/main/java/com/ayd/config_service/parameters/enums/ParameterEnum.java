6b0f41c517ef2ac17d5dc1a94b7eb335f9fd6f7c
FernandoJRR <fercitotron@gmail.com>
2025-05-12 18:57:26 -0600

    refactor: change nombre default empresa

--- config-service/src/main/java/com/ayd/config_service/parameters/enums/ParameterEnum.java
+++ config-service/src/main/java/com/ayd/config_service/parameters/enums/ParameterEnum.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ParameterEnum {
-    NOMBRE_EMPRESA ("nombre_empresa", "Golfito Patito", "Nombre de la Empresa"),
+    NOMBRE_EMPRESA ("nombre_empresa", "Golf Course", "Nombre de la Empresa"),
     NIT_EMPRESA ("nit_empresa", "17678426", "NIT de la Empresa"),
     REGIMEN_EMPRESA ("regimen_empresa", "{name: \"peq\", \"value\": 5}", "Regimen Fiscal de la Empresa"),
     DIAS_VACACIONES ("dias_vacaciones", "15", "Dias de Vacaciones"),
